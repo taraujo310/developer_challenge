e0cb4772981a798f7ed68bb75bc16a8691dd0f2d
Thiago Ururay <taraujo@id.uff.br>
2017-07-17 15:38:53 -0300

    responsive menu

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,12 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-u = User.first
+u = User.first_or_create(
+      name: 'adminsitrator',
+      email: 'admin@admin.com',
+      password: '123456',
+      password_confirmation: '123456'
+    )
 
 6.times do
   Post.create(
