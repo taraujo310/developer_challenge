cd122a474dd7998e47776f97411fd426dbcb99f3
Thiago Ururay <taraujo@id.uff.br>
2017-07-17 18:12:57 -0300

    changing order of posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,7 +5,7 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.json
   def home
-    @posts = Post.paginate(:page => params[:page], :per_page => 5)
+    @posts = Post.paginate(:page => params[:page], :per_page => 5).order(created_at: :desc)
   end
 
   # GET /posts
